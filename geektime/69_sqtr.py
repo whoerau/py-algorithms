1d734b7f45ae9e85de719f60f84b73037223c20c
Anonymous <Anonymous>
2020-10-31 02:05:46 -0700

    [add] 69_sqtr.py

--- geektime/69_sqtr.py
+++ geektime/69_sqtr.py
@@ -1,17 +1,23 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        if x <= 1:
+            return x
         left, right = 0, x
+        rsp = -1
         while left <= right:
-            m = (left + right) / 2
-            if m == x / m:
-                return m
-            elif m > x / m:
-                right = m
-            elif m < x / m:
-                left = m
-        return m
+            m = (left + right) // 2
+            if m * m == x:
+                rsp = m
+                break
+            elif m * m > x:
+                right = m - 1
+            elif m * m < x:
+                rsp = m
+                left = m + 1
+        return rsp
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.mySqrt(5))
+    m = s.mySqrt(8)
+    print(type(m), m)
